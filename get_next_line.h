190190bcdad00d5daf5e713a6054d73071dac01d
jkulka <kulkajens82@gmail.com>
2023-04-16 15:42:57 +0200

    chore(get_next_line.h): change default BUFFER_SIZE to 1 feat(get_next_line.h): remove commented out function definition of find_newline()

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jkulka <jkulka@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 11:18:43 by jkulka            #+#    #+#             */
-/*   Updated: 2022/12/05 12:06:32 by jkulka           ###   ########.fr       */
+/*   Updated: 2023/04/16 15:34:29 by jkulka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@
 # include <stdlib.h>
 # include <unistd.h>
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 4
+# define BUFFER_SIZE 1
 #endif
 char	*get_next_line(int fd);
 char  *read_line(int fd, char *str);
 int find_newline(char *buffer, int size);
+// int find_newline(char *buffer);
 void	*ft_memcpy(void *dest, void *src, unsigned int n);
 size_t	ft_strlen(const char *s);
 void copy_data(char **output, char *buffer, int len);
