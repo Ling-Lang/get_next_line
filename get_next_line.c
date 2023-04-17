e7a092b5be189b40ca8b7589b8523dd735f8c9db
jkulka <kulkajens82@gmail.com>
2023-04-17 10:43:36 +0200

    refactor(get_next_line.c): remove commented out code and add whitespace

--- get_next_line.c
+++ get_next_line.c
@@ -6,45 +6,11 @@
 /*   By: jkulka <jkulka@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 11:18:38 by jkulka            #+#    #+#             */
-/*   Updated: 2023/04/16 15:38:54 by jkulka           ###   ########.fr       */
+/*   Updated: 2023/04/17 10:41:44 by jkulka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-// char *get_next_line(int fd) {
-//     static char buffer[BUFFER_SIZE + 1];
-//     char *output = NULL;
-//     int newline_pos;
-//     int read_ret;
-
-//     newline_pos = find_newline(buffer, BUFFER_SIZE);
-//     if (newline_pos >= 0) {
-//         copy_data(&output, buffer, newline_pos + 1);
-//         ft_memmove(buffer, buffer + newline_pos + 1, BUFFER_SIZE - newline_pos);
-//         return output;
-//     }
-
-//     while ((read_ret = read(fd, buffer, BUFFER_SIZE)) > 0) {
-//         buffer[read_ret] = '\0';
-
-//         newline_pos = find_newline(buffer, read_ret);
-//         if (newline_pos >= 0) {
-//             copy_data(&output, buffer, newline_pos + 1);
-//             ft_memmove(buffer, buffer + newline_pos + 1, read_ret - newline_pos);
-//             return output;
-//         } else {
-//             copy_data(&output, buffer, read_ret);
-//         }
-//     }
-
-//     if (read_ret == -1) {
-//         free(output);
-//         return NULL;
-//     }
-
-//     return output;
-// }
 
 char	*get_next_line(int fd)
 {
@@ -61,7 +27,9 @@ char	*get_next_line(int fd)
 		ft_memmove(buffer, buffer + newline_pos + 1, BUFFER_SIZE - newline_pos);
 		return (output);
 	}
-	while ((read_ret = read(fd, buffer, BUFFER_SIZE)) > 0)
+	
+	read_ret = read(fd, buffer, BUFFER_SIZE);
+	while (read_ret > 0)
 	{
 		buffer[read_ret] = '\0';
 		newline_pos = find_newline(buffer, read_ret);
@@ -75,14 +43,18 @@ char	*get_next_line(int fd)
 		{
 			copy_data(&output, buffer, read_ret);
 		}
+		read_ret = read(fd, buffer, BUFFER_SIZE);
 	}
+	
 	if (read_ret == -1)
 	{
 		free(output);
 		return (NULL);
 	}
+	
 	return (output);
 }
+
 
 
 
